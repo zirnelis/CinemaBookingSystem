a29010311f48d284c782f3b6bacd863971010785
Elīna <s4_kuzmin_e@venta.lv>
2018-02-12 10:59:39 +0200

    Main and User Class created. Need to be done. For now shows only login window and creates user

---
+++ src/cinemabookingsystem/CinemaBookingSystem.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cinemabookingsystem;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Elina
+ */
+public class CinemaBookingSystem extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        /**
+         * "Hello world button form example"
+         */
+//        Button btn = new Button();
+//        Button btn2 = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn2.setText("TestButton");
+//        call Action when button is pressed
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+            // Action method defined
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+        // Adding Grid to Scene
+        GridPane loginGrid = new GridPane();
+        loginGrid.setAlignment(Pos.CENTER);
+        loginGrid.setHgap(10);
+        loginGrid.setVgap(10);
+        loginGrid.setPadding(new Insets(25, 25, 25, 25));
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        root.getChildren().add(btn2);
+        
+//        Scene scene = new Scene(root, 850, 600);
+        
+        Scene loginScene = new Scene(loginGrid, 850, 600);
+        
+        primaryStage.setTitle("Cinema Booking System");
+        primaryStage.setScene(loginScene);
+        
+        /**
+         * Adding "Cinema Booking system" title
+         * Username and Password field with titles
+         */
+        
+        Text scenetitle = new Text("Welcome to Cinema Booking System");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
+        loginGrid.add(scenetitle, 0, 0, 2, 1); //what means this numbers???
+        
+        Label username = new Label("User Name:");
+        loginGrid.add(username, 0, 1);
+        
+        TextField userTextField = new TextField();
+        loginGrid.add(userTextField, 1, 1);
+        
+        Label password = new Label("Password:");
+        loginGrid.add(password, 0, 2);
+        
+        TextField passTextField = new TextField();
+        loginGrid.add(passTextField, 1, 2);
+        
+        /**
+         * Adding Submit button
+         */
+        
+        Button submitBtn = new Button("Submit");
+        submitBtn.setAlignment(Pos.CENTER_RIGHT);
+        loginGrid.add(submitBtn, 1, 4);
+        
+        /**
+         * Adding Create button
+         */
+        
+        Button createBtn = new Button("Create");
+        createBtn.setAlignment(Pos.CENTER_LEFT);
+        loginGrid.add(createBtn, 2, 4);
+        
+        
+//        HBox hbBtn = new HBox(10);
+//        hbBtn.setAlignment(Pos.CENTER);
+//        hbBtn.getChildren().add(submitBtn);
+//        grid.add(hbBtn, 1, 4);
+        
+        primaryStage.show();
+        
+        /**
+         * Make new Scene for app
+         */
+        
+        GridPane mainGrid = new GridPane();
+        mainGrid.setAlignment(Pos.CENTER);
+        mainGrid.setHgap(10);
+        mainGrid.setVgap(10);
+        mainGrid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Scene mainScene = new Scene(mainGrid, 950, 600);
+        
+        
+        /**
+         * Create button creates new user and writes user`s info into file
+         */
+        
+        createBtn.setOnAction(new EventHandler<ActionEvent>(){
+            
+            User usr = new User(userTextField.getText(), passTextField.getText());
+            
+            @Override
+            public void handle(ActionEvent event) {
+                try {
+                    usr.writeNewUser(usr);
+                } catch (IOException ex) {
+                    Logger.getLogger(CinemaBookingSystem.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }); 
+        
+        /**
+         * If Username and Password is correct, clear window
+         */
+        submitBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                
+                /**
+                 * Write code for User info checking,
+                 * but first make new User!!
+                 */
+                primaryStage.setScene(mainScene);
+            }
+        });
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        launch(args);
+        
+    }
+    
+}
---
+++ src/cinemabookingsystem/User.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cinemabookingsystem;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Elina
+ */
+public class User {
+    public String name;
+    public String password;
+
+    
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+
+    }
+
+    User() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+    /**
+     * Make new userst.txt file
+     * If exists:
+     * Write user info into .txt file
+     */
+    
+    public void writeNewUser(User usr) throws IOException {
+        PrintWriter pw = null;
+        
+        try {
+            pw = new PrintWriter("users.txt");
+            BufferedWriter bw = new BufferedWriter(pw);
+            bw.write(usr.getName());
+//            bw.write("Name");
+            System.out.println("Name has been written");
+            bw.newLine();
+            bw.write(usr.getPassword());
+//            bw.write("Pass");
+            System.out.println("Password has been written");
+
+//            pw.close();
+            
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
+    }
+    
+    
+}
